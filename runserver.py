330fc588905376b3a6c00e71405ca6027d481c56
Philipp Spinnler <philipp@spinnler.ch>
2014-06-13 08:24:15 +0200

    Integrated tornado webserver

--- runserver.py
+++ runserver.py
@@ -1,7 +1,18 @@
 from flaskboilerplate import app
 
+# for tornado integration
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
 # the toolbar is only enabled in debug mode:
 app.debug = True
 # toolbar = DebugToolbarExtension(coreApp)
 
-app.run(debug=True)
+# start with Flask
+#app.run(debug=True)
+
+# start with Tornado
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
