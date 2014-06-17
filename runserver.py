3d10ec757182f7997fe0779985a425dc9386297f
Thomas Spycher <me@tspycher.com>
2014-06-17 15:14:32 +0200

    added support for ssl

--- runserver.py
+++ runserver.py
@@ -1,5 +1,6 @@
 #import logging
 import argparse
+import os
 from flaskboilerplate import create_app
 from werkzeug._internal import _log
 
@@ -11,12 +12,12 @@ from tornado.ioloop import IOLoop
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Staring Flask based RESTful Server')
-    parser.add_argument('port', metavar='Port', type=int, nargs='?', default=5000,
-                       help='port to run the application')
-    parser.add_argument('--env', '-e', dest='environment', action='store',
-                   default='dev',
-                   help='type of environment')
+    parser.add_argument('port', metavar='Port', type=int, nargs='?', default=5000, help='port to run the application')
+    parser.add_argument('--env', '-e', dest='environment', action='store', default='dev', help='type of environment')
     parser.add_argument('--tornado', '-t', dest='tornado', action='store_true', help='run the server as tornado wsgi')
+    parser.add_argument('--ssl', '-s', dest='use_ssl', action='store_true', help='run server with ssl certs')
+    parser.add_argument('--ssl-certfile', '-c', dest='ssl_certfile', action='store', default='server.crt', help='ssl certificate file')
+    parser.add_argument('--ssl-keyfile', '-k', dest='ssl_keyfile', action='store', default='server.key', help='ssl key file')
     args = parser.parse_args()
 
     app = create_app()
@@ -29,7 +30,13 @@ if __name__ == "__main__":
     if args.tornado:
         try:
             _log('info', " * Starting Tornado Server")
-            http_server = HTTPServer(WSGIContainer(app))
+            if args.use_ssl:
+                ssl_options={'certfile': os.path.join(args.ssl_certfile), 'keyfile': os.path.join(args.ssl_keyfile)}
+                #from flask_sslify import SSLify
+                #sslify = SSLify(app, permanent=True)
+            else:
+                ssl_options=None
+            http_server = HTTPServer(WSGIContainer(app), ssl_options=ssl_options)
             http_server.listen(args.port)
             IOLoop.instance().start()
         except KeyboardInterrupt as e:
