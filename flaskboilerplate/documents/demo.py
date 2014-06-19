7b1710e95c9c053277359d9342dc26bd92d2cde4
Thomas Spycher <me@tspycher.com>
2014-06-19 11:23:33 +0200

    cleaning up the Document

--- flaskboilerplate/documents/demo.py
+++ flaskboilerplate/documents/demo.py
@@ -1,6 +1,8 @@
 from . import odm as db
 from flask.ext.restful import fields
 from basedocument import Basedocument
+#from mongoengine import queryset_manager
+
 
 class Demo(Basedocument):
     resource_fields = {
@@ -13,3 +15,8 @@ class Demo(Basedocument):
     email = db.StringField(required=True)
     first_name = db.StringField(max_length=50)
     last_name = db.StringField(max_length=50)
+
+    #@queryset_manager
+    #def objects(doc_cls, queryset):
+    #    ''' Override this method to customize the default query '''
+    #    return queryset
