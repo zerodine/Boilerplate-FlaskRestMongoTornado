0ea75ce8ab4c151a8b47e75e4763313da0aab6b5
Philipp Spinnler <philipp@spinnler.ch>
2014-06-19 10:15:06 +0200

    Fixed a little slash-missing bug

--- flaskboilerplate/resources/__init__.py
+++ flaskboilerplate/resources/__init__.py
@@ -5,5 +5,5 @@ from demo import Demo
 api = getattr(g, '_api', None)
 
 # Define the resources and routes
-api.add_resource(Demo, '/demo/<string:_email>', 'demos')
-#api.add_resource(AnotherResource, '/anotherresource/<string:name>', 'anotherressource')
+api.add_resource(Demo, '/demo/<string:_email>', '/demos')
+#api.add_resource(AnotherResource, '/anotherresource/<string:name>', '/anotherressource')
